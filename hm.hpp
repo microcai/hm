52e158c4e53a58629eca54a1245fd22a7145ee4b
microcai <microcai@fedoraproject.org>
2012-09-28 21:39:43 +0800

    add main_client(const char * arg1,const char * arg2,...) to hm.hpp
    Signed-off-by: microcai <microcai@fedoraproject.org>

--- hm.hpp
+++ hm.hpp
@@ -19,5 +19,6 @@ typedef int (*MAINFUNC)(int argc, const char * argv[]);
 int main_book(int argc , const char * argv[]);
 
 int main_client(int argc , const char * argv[]);
+int main_client(const char * arg1,const char * arg2,...);
 
 int main_init(int argc , const char * argv[]);
