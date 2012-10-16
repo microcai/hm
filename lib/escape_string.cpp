c25b9819756c3c4f2a6fb9a8a995d37b0b5b4d22
microcai <microcai@fedoraproject.org>
2012-10-17 00:42:43 +0800

    add escape string

---
+++ lib/escape_string.cpp
@@ -0,0 +1,37 @@
+#include "pch.hpp"
+#include "hm.hpp"
+// http://www.ietf.org/rfc/rfc2396.txt
+// section 2.3
+static const char unreserved_chars[] =
+		// when determining if a url needs encoding
+		// % should be ok
+		"%+"
+		// reserved
+		";?:@=&,$/"
+		// unreserved (special characters) ' excluded,
+		// since some buggy trackers fail with those
+		"-_!.~*()"
+		// unreserved (alphanumerics)
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+		"0123456789";
+
+static const char hex_chars[] = "0123456789abcdef";
+
+std::string escape_string(std::string str)
+{
+	std::string ret;
+	for (auto c : str)
+	{
+		if (std::strchr(unreserved_chars, c) && c != 0)
+		{
+				ret += c;
+		}
+		else
+		{
+				ret += '%';
+				ret += hex_chars[((unsigned char)c) >> 4];
+				ret += hex_chars[((unsigned char)c) & 15];
+		}
+	}
+	return ret;
+}
